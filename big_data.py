b50581ae5549ca4dc426c682a6aef52042292839
michaeldolan123 <mqdolan24@students.bmchs.org>
2022-01-14 08:46:20 -0600

    finished

--- big_data.py
+++ big_data.py
@@ -1,6 +1,18 @@
 import csv 
-with open('BigData2016(1).csv', newline='') as csvfile:
+list = ['ARD2', 'BEAV', 'BOIS', 'CENT', 'NRMN', 'STIL', 'TISH', 'TULN', 'WOOD']
+data = open('/home/mdolan/Desktop/git_prog/BigData2016 (1).csv', newline='') 
+with data as csvfile:
     reader = csv.DictReader(csvfile)
     station_list = []
-    for station in reader:
-        if station['STID'] 
+    for part in list:
+        maxes = -996.0
+        mins = 996.0
+        data.seek(0)
+        for num in reader:
+            if num['STID'] == part:
+                if float(num['TMAX']) != 996.00 and float(num['TMAX']) != -996.00:
+                    if float(num['TMAX']) > maxes:
+                        maxes = float(num['TMAX'])
+                    if float(num['TMIN']) < mins:
+                        mins = float(num['TMIN'])
+        print(f'{part}; max:{maxes}, min:{mins}.')
