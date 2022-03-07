0525c8fa4496b0dcb01c2ae89a3eb5d27916cca0
michaeldolan123 <mqdolan24@students.bmchs.org>
2022-03-07 08:52:49 -0600

    started ap black jack prog

--- ap.py
+++ ap.py
@@ -18,18 +18,41 @@ for card in deck_of_cards:
         shuffle_hand.append(card)
 
 def stat_of_deck(deck, deal1c, use1c, use2c):
-    new_deck = deck
-    user_points = 0
-    dealer_points = 0
+    if deal1c == 'a':
+        deal1c = int(input('what is the value of the dealers ace? >'))
+    if use1c == 'a':
+        use1c = int(input('what is the value of your ace? >'))
+    if use2c == 'a':
+        use2c = int(input('what is the value of your ace? >'))
+    user_points = int(use1c) + int(use2c)
+    user_odds = 0
+    dealer_odds = 0
     for card in deck:
-        
-stat_of_deck(shuffle_hand)
+        if card == 'a':
+            if int(deal1c) + 11 > user_points:
+                dealer_odds += 1
+            if int(deal1c) + 11 < user_points:
+                user_odds += 1
+            if int(deal1c) + 1 > user_points:
+                dealer_odds += 1
+            if int(deal1c) + 1 < user_points:
+                user_odds += 1
+        else:
+            if int(deal1c) + int(card) > user_points:
+                dealer_odds += 1
+            if int(deal1c) + int(card) < user_points:
+                user_odds += 1
+            if int(deal1c) + int(card) > user_points:
+                dealer_odds += 1
+            if int(deal1c) + int(card) < user_points:
+                user_odds += 1
+    sum_of_points = user_odds + dealer_odds
+    deal_odds_o_beating = dealer_odds / sum_of_points 
+    deal_odds_o_beating = deal_odds_o_beating * 100
+    use_odds_o_beating = user_odds / sum_of_points 
+    use_odds_o_beating = use_odds_o_beating * 100
 
-
-
-
-
-
+    print(f'Your cance of a win is {use_odds_o_beating}%')
 #print odds
 dealer_1st_card = input('What is the dealers first card? >')
 for card in shuffle_hand:
@@ -49,8 +72,10 @@ for card in shuffle_hand:
         shuffle_hand.remove(card)
         break
 
+
+
 while True:
-    #print odds
+    stat_of_deck(shuffle_hand, dealer_1st_card, user_first_card, user_second_card)
     question = input('Did you get another card? y or n >')
     if question == 'y':
         user_n_card = input('What was that card >')
@@ -60,7 +85,7 @@ while True:
                 break
     else:
         break
-
+while True:
     question = input('Did the dealer get another card? y or n >')
     if question == 'y':
         dealer_n_card = input('What was that card >')
@@ -70,4 +95,3 @@ while True:
                 break
     else:
         break
-
