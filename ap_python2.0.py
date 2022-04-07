cc8604b3d04452730dd4cb04758d1e65b7abe632
michaeldolan123 <mqdolan24@students.bmchs.org>
2022-04-07 08:44:56 -0500

    started 3 var of bj prog

--- ap_python2.0.py
+++ ap_python2.0.py
@@ -1,7 +1,24 @@
 suites = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10', 'a']
 deck = []
+spair_deck = []
+
 how_many_decks = int(input('How many decks are you using')) * 4
 for card in suites:
     for num in range(how_many_decks):
         deck.append(card)
-print(deck)
+
+def odds(user1, user2, user_spair, deal1, deal_other):
+    deck.remove(user1)
+    deck.remove(user2)
+    deck.remove(deal1)
+    
+    while True:
+
+    
+
+
+while True:
+    user_1_card = input('what is your first card')
+    user_2_card = input('what is your second card')
+    dealer_1_card = input('what is the dealers first card')
+    odds(user_1_card, user_2_card, 0, dealer_1_card, 0)
