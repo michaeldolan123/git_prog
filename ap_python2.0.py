66d2a83c20d1f16857d29c6bf32c1750fbdcdf75
michaeldolan123 <mqdolan24@students.bmchs.org>
2022-04-11 08:54:28 -0500

    started 3 var of bj prog

--- ap_python2.0.py
+++ ap_python2.0.py
@@ -1,8 +1,8 @@
-suites = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10', 'a']
+suites = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '10', '10', '10', '11']
 deck = []
 spair_deck = []
 
-how_many_decks = int(input('How many decks are you using')) * 4
+how_many_decks = int(input('How many decks are you using? >')) * 4
 for card in suites:
     for num in range(how_many_decks):
         deck.append(card)
@@ -11,14 +11,22 @@ def odds(user1, user2, user_spair, deal1, deal_other):
     deck.remove(user1)
     deck.remove(user2)
     deck.remove(deal1)
-    
+    positive_odds = 0
+    negative_odds = 0
     while True:
-
-    
-
+        if int(user1) + int(user2) == 21:
+            break
+        for card in deck:
+            if int(user1) + int(user2) + int(card) <= 21:
+                positive_odds += 1
+            else:
+                negative_odds += 1
+        sum = negative_odds + positive_odds
+        print(f'you have a {negative_odds / sum * 100}% chance of busting')
+        break
 
 while True:
-    user_1_card = input('what is your first card')
-    user_2_card = input('what is your second card')
-    dealer_1_card = input('what is the dealers first card')
+    user_1_card = input('what is your first card >')
+    user_2_card = input('what is your second card >')
+    dealer_1_card = input('what is the dealers first card >')
     odds(user_1_card, user_2_card, 0, dealer_1_card, 0)
